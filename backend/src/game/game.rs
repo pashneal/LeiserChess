9635c7668da318b86af4079ab2e7b73c7d01e89f
Neal Powell <pashneal@gmail.com>
2024-08-15 10:20:17 -0400

    Notes for unifying interfaces

--- backend/src/game/game.rs
+++ backend/src/game/game.rs
@@ -7,6 +7,9 @@ use crate::action::*;
 pub trait Board : Indexable + OptimizedIndexable + Parseable + HumanReadable + Default {}
 pub trait GameAction<T : OptimizedIndexable> : OptimizedAction<T> + Parseable +  Default {}
 
+/// TODO: Somehow this structure should unify the interfaces
+/// of Optimized and non optimized interfaces so that it is easy to 
+/// test both implementations, or switch between them
 pub struct Game<B : Board, A : GameAction<B> > {
     current_board : B,
     history : [B; MAX_HISTORY_LENGTH],
